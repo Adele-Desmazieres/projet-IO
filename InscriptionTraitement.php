4218f8c513e285e7b065dc0f5e4759f2d71cdae5
LuZogg <75303569+LuZogg@users.noreply.github.com>
2021-04-01 18:32:54 +0200

    Add files via upload

--- InscriptionTraitement.php
+++ InscriptionTraitement.php
@@ -1,44 +1,18 @@
 <?php
+ require_once("BibliothequeFonctions.php");
 
-    function preTraitement($champ) {
-        if(!empty($champ)) {
-            $champ=trim($champ);
-            $champ=htmlspecialchars($champ);
-        }
-        return $champ;
-    }
-
-    function probleme() {
-        if(empty($_REQUEST['pseudo']) && empty($_REQUEST['mdp'])){
-            $ret=array('pseudo','mot de passe');
-        }
-        if(empty($_REQUEST['mdp']) && !empty($_REQUEST['pseudo'])) {
-            $ret=array('mot de passe');
-        } 
-        if(empty($_REQUEST['pseudo']) && !empty($_REQUEST['mdp'])){
-            $ret=array('pseudo');
-        }
-        if(!empty($_REQUEST['pseudo']) && !empty($_REQUEST['mdp'])){
-            $ret=array();
-        }
-        return $ret;
-    }
-
-    function reponse($arr){
-        foreach($arr as $val) {
-            echo "<p>Le ".$val." est incorrect</p>";
+    // affiche le contenu de $arr
+function reponseRecap($arr) {
+    foreach ($arr as $clef => $val) {
+        if ($clef != "mdp") {
+            echo "<p>".$clef." : ".$val."</p>";
         }
     }
+}
 
-    if(count(probleme())==0) {
-        echo "<h1>Recapitulatif</h1><br>";
-        echo "<p>Pseudo : ".$_REQUEST["pseudo"]."</p>";
-        echo "<p>Mot de passe : ".$_REQUEST["mdp"]."</p>";
-    } else {
-        reponse(probleme());
-    }
-    echo "<form action='inscription.html'> <input type='submit' size='20' value='Retour'> </form>"
-
-
-
-    ?>
+if(count(probleme($_POST))==0) {
+    reponseRecap($_POST);
+}
+echo "<form action='inscription.html'> <input type='submit' size='20' value='Retour'> </form>"
+?>
+<form action='FilActualite.php' method='post'> <input type='submit' size='20' value="Accéder à la page d'accueil"> <input type=hidden name='pseudo' value="<?php echo $_REQUEST['pseudo']; ?>"> </form>

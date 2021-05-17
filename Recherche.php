f3ea8ed85320deb6deeab52b39a5c5bcf213b47a
Delta-Chat <63289417+Delta-Chat@users.noreply.github.com>
2021-05-17 11:17:08 +0200

    Add files via upload

---
+++ Recherche.php
@@ -0,0 +1,139 @@
+<?php 
+require_once("BibliothequeFonctions.php");
+teteDePage("Noddle : recherche");
+
+
+?>
+
+<h1>Rechercher</h1>
+
+<h2>Rechercher un compte</h2>
+<div>
+<form action="Recherche.php" method="POST">
+	<p>Pseudo <input type="search" name="pseudo" placeholder="Pseudo" value=
+		<?php 
+		if (isset($_POST["pseudo"])) {
+			echo "\"".$_POST["pseudo"]."\"";
+		}
+		?>>
+	</p>
+	<p>Mail <input type="search" name="mail" placeholder="Mail" value=
+		<?php 
+		if (isset($_POST["mail"])) {
+			echo "\"".$_POST["mail"]."\"";
+		}
+		?>>
+	</p>
+	<input type="hidden" name="table" value="Users">
+	<p><input type="submit" value="Rechercher"></p>
+</form>
+</div>
+
+<h2>Rechercher une publication</h2>
+<div>
+<form action="Recherche.php" method="POST">
+	<p>Description <input type="search" name="description" placeholder="Description de la publication" value=<?php // cette injection de php permet de préremplir la recherche actuelle avec la dernière
+		if (isset($_POST["description"])) {
+			echo "\"".$_POST["description"]."\"";
+		}
+	?>>
+	</p>
+	<p>Auteur <input type="search" name="auteur" placeholder="Pseudo de l'auteur" value=<?php 
+		if (isset($_POST["auteur"])) {
+			echo "\"".$_POST["auteur"]."\"";
+		}
+	?>>
+	</p>
+	<p>Type de fichier
+		<ul>
+		<?php
+		$extensions = array('.odt', '.pdf', '.jpg', '.jpeg', '.png');
+		foreach ($extensions as $ext) { ?>
+			<li><input 
+					type="checkbox" 
+					name="type[]" 
+					value=<?php echo "\"".$ext."\""; // une valeur dans le tableau des extensions
+					// qu'on pré-coche si précédemment cochée
+					if (isset($_POST["type"])) {
+						foreach ($_POST["type"] as $precedenteCochee) {
+							if ($precedenteCochee == $ext) {
+								echo " checked";
+							}
+						}
+					// sauf si on l'initialise avec toutes de cochées
+					} else {
+						echo " checked";
+					}
+					?> > 
+				<?php echo substr($ext, 1);?> </li>
+		<?php
+		} ?>
+		</ul>
+	</p>
+	<input type="hidden" name="table" value="Publications">
+	<p><input type="submit" value="Rechercher"></p>
+</form>
+</div>
+
+<h2>Résultats de la recherche</h2>
+
+<?php
+
+require_once("RequeteSQL.php");
+
+/*
+echo "BONJOUR<pre>";
+print_r($_POST);
+echo "</pre>";
+echo ($_POST["description"] == NULL)."<br>";
+*/
+
+// on prétraite le tableau POST pour obtenir
+// $attributs : tableau indicé des attributs sélectionnés
+// $table : le string de la table dans laquelle on fera la requete
+// $conditions : tableau associatif des conditions nécessaires à la requête 
+if (isset($_POST["table"])) {
+	$table = $_POST["table"];
+	if ($_POST["table"] == "Users") {
+		$attributs = array("pseudo");
+	} else {
+		$attributs = array("*"); 
+	}
+
+	$conditions = array();
+	foreach ($_POST as $clef => $val) {
+		if ($clef != "table" AND $val != NULL) {
+			$conditions[$clef] = $val;
+		}
+	}
+}
+
+// affiche la requete sql
+$requete = creationRequete($attributs, $table, $conditions);
+echo "<br>".$requete;
+
+// crée la connexion sql
+$connex = sqlConnexion();
+if ($connex) {
+	$resultat = mysqli_query($connex, $requete);
+	$ligneResultat = mysqli_fetch_assoc($resultat);
+
+	// parcourt et affiche toutes les publications
+	if ($table == "Publications") {
+		afficherPublications($ligneResultat, $resultat);
+	} else {
+		while($ligneResultat) {
+			afficherApercuCompte($ligneResultat);
+		}
+	}
+	
+}
+
+?>
+
+
+
+
+<?php
+piedDePage();
+?>
---
+++ RequeteSQL.php
@@ -0,0 +1,89 @@
+<?php
+require_once("BibliothequeFonctions.php");
+teteDePage("Requete SQL");
+
+// tente de se connecter à la BDD, et renvoie une erreur sinon
+function sqlConnexion() {
+	// renvoie un objet de connexion, ou false si echec
+	// prend en argument : serveur, utilisateur, mdp, nom de la BDD
+	$connex = mysqli_connect('localhost', 'root', '', 'IO_TEST');
+	if (!$connex) {
+		echo "<br>Erreur : impossible de se connecter à la BDD (".$mysqli_connect_error().").";
+	}
+	return $connex;
+}
+
+// renvoie un String correspondant à cette requete :
+// SELECT $attributs FROM $table WHERE $clef1 LIKE "%$val1%" AND $clef2=$val2 AND...
+// fonctionne uniquement avec une tableau contenant String et int
+// le tableau $conditions peut contenir un tableau indicé, ce qui donnera :
+// ... AND ($clef=$val1 OR $clef=$val2)
+function creationRequete($attributs, $table, $conditions) {
+	$sqlRequete = "SELECT";
+	foreach ($attributs as $att) {
+		$sqlRequete = $sqlRequete." $att,"; // SELECT "att1", "att2",
+	}
+	// on retire la virgule en trop
+	$sqlRequete = substr($sqlRequete, 0, strlen($sqlRequete)-1);
+	$sqlRequete = $sqlRequete." FROM ".$table." WHERE";
+
+	// on ajoute les conditions séparées par AND
+	foreach ($conditions as $clef => $val) {
+
+		// si c'est un string : clef LIKE "%val%" AND
+		if (is_string($val)) {
+			$sqlRequete = $sqlRequete." ".$clef." LIKE \"%".$val."%\" AND";
+
+		// si c'est un tableau : ( clef=valInt1 OR clef LIKE "%valInt2%" ) AND
+		} else if (is_array($val)) {
+			$sqlRequete = $sqlRequete." (";
+			foreach ($val as $valInterne) {
+				//echo $valInterne;
+				if (is_string($valInterne)) {
+					$sqlRequete = $sqlRequete." ".$clef." LIKE \"%".$valInterne."%\" OR";
+				} else {
+					$sqlRequete = $sqlRequete." ".$clef."=".$valInterne." OR";
+				}
+			}
+			$sqlRequete = substr($sqlRequete, 0, strlen($sqlRequete)-2);
+			$sqlRequete = $sqlRequete.") AND";
+
+		// sinon c'est un entier : clef="val" AND
+		} else {
+			$sqlRequete = $sqlRequete." ".$clef."=".$val." AND";
+		}
+	}
+	// on retire le AND de trop et on met le point-virgule final
+	$sqlRequete = substr($sqlRequete, 0, strlen($sqlRequete)-4);
+	$sqlRequete = $sqlRequete.";";
+	return $sqlRequete;
+}
+
+function afficherApercuCompte($donneesCompte) { 
+	$nbrAbonnés = sqli_query();
+
+	?>
+	<div>
+		<p><?php echo $donneesCompte["pseudo"];?></p>
+		<p>Abonnés <?php echo $donneesCompte[]
+	</div>
+	<?php
+}
+
+
+/*
+$userid = array(1, 3);
+$conditions = array("pseudo" => "delta", "userid" => $userid);
+$attributs = array("userid", "pseudo");
+$table = "Users";
+echo "<br><u>inputs :</u><pre>";
+print_r($attributs);
+echo $table.", <br>";
+print_r($conditions);
+echo "</pre>";
+echo "<br><u>output :</u> ".creationRequete($attributs, $table, $conditions);
+
+*/
+ 
+piedDePage();
+?>
